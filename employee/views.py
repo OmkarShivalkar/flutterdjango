43a081452807e0bc0c24ea15166b254a8a2c306b
omkar <omkarshivalkar10@gmail.com>
2021-12-29 10:54:43 +0530

    mew proj

---
+++ employee/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from employee.models import Employee
+from .serializer import EmployeeSerializer
+from rest_framework import serializers, viewsets
+
+# Create your views here.
+class Employee(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
